3d8e9ffa46c7dcabcc16653c411f9e6bc0f9167e
Cristinel Pavel <cristinel.pavel26@gmail.com>
2021-08-15 17:57:53 +0200

    First commit

---
+++ ch04/tacos-sd-mongodb/src/main/java/tacos/web/DesignTacoController.java
@@ -0,0 +1,71 @@
+package tacos.web;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import lombok.extern.slf4j.Slf4j;
+import tacos.Ingredient;
+import tacos.Ingredient.Type;
+import tacos.Taco;
+import tacos.data.IngredientRepository;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+@SessionAttributes("tacoOrder")
+public class DesignTacoController {
+
+	private final IngredientRepository ingredientRepo;
+
+	@Autowired
+	public DesignTacoController(IngredientRepository ingredientRepo) {
+		this.ingredientRepo = ingredientRepo;
+	}
+
+	@ModelAttribute
+	public void addIngredientsToModel(Model model) {
+		Iterable<Ingredient> ingredients = ingredientRepo.findAll();
+		Type[] types = Ingredient.Type.values();
+		for (Type type : types) {
+			model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+		}
+	}
+
+
+	@GetMapping
+	public String showDesignForm(Model model) {
+		model.addAttribute("taco", new Taco());
+
+		return "design";
+	}
+
+
+	@PostMapping
+	public String processTaco(@Valid @ModelAttribute("taco") Taco taco, Errors errors) {
+		if (errors.hasErrors()) {
+			return "design";
+		}
+
+		// Save the taco...
+		// We'll do this in chapter 3
+		log.info("Processing taco: " + taco);
+
+		return "redirect:/orders/current";
+	}
+
+
+	private Iterable<Ingredient> filterByType(Iterable<Ingredient> ingredientsList, Type type) {
+		List<Ingredient> ingredients = (List<Ingredient>) ingredientsList;
+		
+		return ingredients.stream().filter(x -> x.getType().equals(type)).collect(Collectors.toList());
+	}
+}
---
+++ ch04/tacos-sd-mongodb/src/main/java/tacos/web/OrderController.java
@@ -0,0 +1,49 @@
+package tacos.web;
+
+import javax.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.support.SessionStatus;
+import lombok.extern.slf4j.Slf4j;
+import tacos.TacoOrder;
+import tacos.data.OrderRepository;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+
+	private OrderRepository orderRepo;
+
+	public OrderController(OrderRepository orderRepo) {
+		this.orderRepo = orderRepo;
+	}
+
+	@GetMapping("/current")
+	public String orderForm(Model model) {
+		model.addAttribute("tacoOrder", new TacoOrder());
+
+		return "orderForm";
+	}
+
+
+	@PostMapping
+	public String processOrder(@Valid TacoOrder tacoOrder, Errors errors,
+			SessionStatus sessionStatus) {
+		
+		if (errors.hasErrors()) {
+			return "orderForm";
+		}
+		
+		orderRepo.save(tacoOrder);
+		sessionStatus.setComplete();
+
+		log.info("Order submitted: " + tacoOrder);
+
+		return "redirect:/";
+	}
+}
