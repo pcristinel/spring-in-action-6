753b7c30db4c42927c81f8791d9924630c534101
Cristinel Pavel Alergus <pcristinel@Cristinels-MacBook-Pro.local>
2025-07-20 23:16:53 +0200

    starting ch05

---
+++ ch05-security/src/main/java/com/cristinelpavel/sia6_ch05_security/Sia6Ch05SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.cristinelpavel.sia6_ch05_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sia6Ch05SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sia6Ch05SecurityApplication.class, args);
+	}
+
+}
---
+++ ch05-security/src/test/java/com/cristinelpavel/sia6_ch05_security/Sia6Ch05SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristinelpavel.sia6_ch05_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sia6Ch05SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
